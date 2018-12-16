0b990ed67346038142df3f2657b6071c1cbd8f31
Jeroen Geusebroek <me@jeroengeusebroek.nl>
2018-12-16 13:58:25 +0100

    Vagrant support

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,10 +4,14 @@ if [[ $ANSIBLE_CLEANUP  =~ true || $ANSIBLE_CLEANUP =~ 1 || $ANSIBLE_CLEANUP =~ 
   # Uninstall Ansible and remove PPA.
   apt -y remove --purge ansible
   apt-add-repository --remove ppa:ansible/ansible
+
+  # Delete Ansible leftovers in home directory
+  rm -rf ~/.ansible*
 fi
 
 # Apt cleanup.
-apt autoremove
+apt clean
+apt autoremove -y
 apt update
 
 # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
