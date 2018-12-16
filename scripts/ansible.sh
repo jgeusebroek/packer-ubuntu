530169c42f21a718ef03308f63ecb09f493ae8f0
Jeroen Geusebroek <me@jeroengeusebroek.nl>
2018-12-16 13:43:29 +0100

    Do not update when installing Ansible

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 # Install Ansible repository.
-apt -y update && apt-get -y upgrade
+apt -y update
 apt -y install software-properties-common
 apt-add-repository ppa:ansible/ansible
 
